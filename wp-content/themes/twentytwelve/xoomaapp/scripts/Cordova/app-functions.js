3f935b4a2d94eaa00cfa2383bdb28868f113c01c
Deepak Prabhudesai <deepak@ajency.in>
2014-12-18 17:34:26 +0530

    no message

--- wp-content/themes/twentytwelve/xoomaapp/scripts/Cordova/app-functions.js
+++ wp-content/themes/twentytwelve/xoomaapp/scripts/Cordova/app-functions.js
@@ -1,27 +1,24 @@
-(function() {
-  _.mixin({
-    cordovaHideSplashscreen: function() {
-      return navigator.splashscreen.hide();
-    },
-    enableCordovaBackbuttonNavigation: function() {
-      navigator.app.overrideBackbutton(true);
-      return document.addEventListener("backbutton", _.onDeviceBackButtonClick, false);
-    },
-    disableCordovaBackbuttonNavigation: function() {
-      return navigator.app.overrideBackbutton(false);
-    },
-    onDeviceBackButtonClick: function() {
-      var currentRoute;
-      currentRoute = App.getCurrentRoute();
-      console.log('Fired cordova back button event for ' + currentRoute);
-      if (currentRoute === 'login' || currentRoute === 'profile/personal-info') {
-        navigator.app.exitApp();
-      }
-      return _.removeCordovaBackbuttonEventListener();
-    },
-    removeCordovaBackbuttonEventListener: function() {
-      return document.removeEventListener("backbutton", _.onDeviceBackButtonClick, false);
+_.mixin({
+  cordovaHideSplashscreen: function() {
+    return navigator.splashscreen.hide();
+  },
+  enableCordovaBackbuttonNavigation: function() {
+    navigator.app.overrideBackbutton(true);
+    return document.addEventListener("backbutton", _.onDeviceBackButtonClick, false);
+  },
+  disableCordovaBackbuttonNavigation: function() {
+    return navigator.app.overrideBackbutton(false);
+  },
+  onDeviceBackButtonClick: function() {
+    var currentRoute;
+    currentRoute = App.getCurrentRoute();
+    console.log('Fired cordova back button event for ' + currentRoute);
+    if (currentRoute === 'login' || currentRoute === 'profile/personal-info') {
+      navigator.app.exitApp();
     }
-  });
-
-}).call(this);
+    return _.removeCordovaBackbuttonEventListener();
+  },
+  removeCordovaBackbuttonEventListener: function() {
+    return document.removeEventListener("backbutton", _.onDeviceBackButtonClick, false);
+  }
+});
