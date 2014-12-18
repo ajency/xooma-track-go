0ea6d7c9be5b1985eee4b758d28075939d6e5e67
Deepak Prabhudesai <deepak@ajency.in>
2014-12-18 19:24:24 +0530

    no message

--- wp-content/themes/twentytwelve/xoomaapp/scripts/profile/ProfilePersonalInfoCtrl.js
+++ wp-content/themes/twentytwelve/xoomaapp/scripts/profile/ProfilePersonalInfoCtrl.js
@@ -1,57 +1,54 @@
-(function() {
-  var __hasProp = {}.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+var __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-  App.ProfilePersonalInfoCtrl = (function(_super) {
-    __extends(ProfilePersonalInfoCtrl, _super);
+App.ProfilePersonalInfoCtrl = (function(_super) {
+  __extends(ProfilePersonalInfoCtrl, _super);
 
-    function ProfilePersonalInfoCtrl() {
-      return ProfilePersonalInfoCtrl.__super__.constructor.apply(this, arguments);
+  function ProfilePersonalInfoCtrl() {
+    return ProfilePersonalInfoCtrl.__super__.constructor.apply(this, arguments);
+  }
+
+  ProfilePersonalInfoCtrl.prototype.initialize = function(options) {
+    if (_.onlineStatus() === false) {
+      window.plugins.toast.showLongBottom("Please check your internet connection.");
+    } else {
+      this.user = this._get_user_details();
     }
+    return App.execute("when:fetched", [this.user], (function(_this) {
+      return function() {
+        console.log(_this.user);
+        return _this.show(new App.ProfilePersonalInfoView({
+          model: _this.user
+        }));
+      };
+    })(this));
+  };
 
-    ProfilePersonalInfoCtrl.prototype.initialize = function(options) {
-      if (_.onlineStatus() === false) {
-        window.plugins.toast.showLongBottom("Please check your internet connection.");
-      } else {
-        this.user = this._get_user_details();
+  ProfilePersonalInfoCtrl.prototype._get_user_details = function() {
+    $.ajax({
+      method: 'GET',
+      url: _SITEURL + '/wp-json/profiles/2',
+      data: '',
+      success: function(response) {
+        var response_data;
+        response_data = response;
+        App.currentUser.set('xooma_member_id', response_data.response.xooma_member_id);
+        App.currentUser.set('name', response_data.response.name);
+        App.currentUser.set('email_id', response_data.response.email);
+        App.currentUser.set('image', response_data.response.image);
+        App.currentUser.set('gender', response_data.response.gender);
+        App.currentUser.set('phone_no', response_data.response.phone_no);
+        App.currentUser.set('timezone', response_data.response.timezone);
+        App.currentUser.set('birth_date', response_data.response.birth_date);
+        return App.currentUser.set('user_products', response_data.response.user_products);
+      },
+      error: function(error) {
+        return $('.response_msg').text("Something went wrong");
       }
-      return App.execute("when:fetched", [this.user], (function(_this) {
-        return function() {
-          console.log(_this.user);
-          return _this.show(new App.ProfilePersonalInfoView({
-            model: _this.user
-          }));
-        };
-      })(this));
-    };
+    });
+    return App.currentUser;
+  };
 
-    ProfilePersonalInfoCtrl.prototype._get_user_details = function() {
-      $.ajax({
-        method: 'GET',
-        url: _SITEURL + '/wp-json/profiles/2',
-        data: '',
-        success: function(response) {
-          var response_data;
-          response_data = response;
-          App.currentUser.set('xooma_member_id', response_data.response.xooma_member_id);
-          App.currentUser.set('name', response_data.response.name);
-          App.currentUser.set('email_id', response_data.response.email);
-          App.currentUser.set('image', response_data.response.image);
-          App.currentUser.set('gender', response_data.response.gender);
-          App.currentUser.set('phone_no', response_data.response.phone_no);
-          App.currentUser.set('timezone', response_data.response.timezone);
-          App.currentUser.set('birth_date', response_data.response.birth_date);
-          return App.currentUser.set('user_products', response_data.response.user_products);
-        },
-        error: function(error) {
-          return $('.response_msg').text("Something went wrong");
-        }
-      });
-      return App.currentUser;
-    };
+  return ProfilePersonalInfoCtrl;
 
-    return ProfilePersonalInfoCtrl;
-
-  })(Marionette.RegionController);
-
-}).call(this);
+})(Marionette.RegionController);
--- wp-content/themes/twentytwelve/xoomaapp/scripts/profile/ProfilePersonalInfoView.js
+++ wp-content/themes/twentytwelve/xoomaapp/scripts/profile/ProfilePersonalInfoView.js
@@ -1,68 +1,65 @@
-(function() {
-  var __hasProp = {}.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+var __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-  App.ProfilePersonalInfoView = (function(_super) {
-    __extends(ProfilePersonalInfoView, _super);
+App.ProfilePersonalInfoView = (function(_super) {
+  __extends(ProfilePersonalInfoView, _super);
 
-    function ProfilePersonalInfoView() {
-      return ProfilePersonalInfoView.__super__.constructor.apply(this, arguments);
+  function ProfilePersonalInfoView() {
+    return ProfilePersonalInfoView.__super__.constructor.apply(this, arguments);
+  }
+
+  ProfilePersonalInfoView.prototype.className = 'animated fadeIn';
+
+  ProfilePersonalInfoView.prototype.template = '#profile-personal-info-template';
+
+  ProfilePersonalInfoView.prototype.events = {
+    'click .radio': function(e) {
+      return $('#gender').val($('#' + e.target.id).val());
     }
+  };
 
-    ProfilePersonalInfoView.prototype.className = 'animated fadeIn';
+  ProfilePersonalInfoView.prototype.onShow = function() {
+    this.$el.find("#timezone option[value='" + this.model.get('timezone') + "']").attr("selected", "selected");
+    $("input[name=radio_grp][value=" + this.model.get('gender') + "]").prop('checked', true);
+    $('#gender').val(this.model.get('gender'));
+    return jQuery.validator.addMethod("equalLength", function(value, element) {
+      return this.optional(element) || (parseInt(value.length) === 6);
+    }, "* Enter valid 6 digit Xooma ID");
+  };
 
-    ProfilePersonalInfoView.prototype.template = '#profile-personal-info-template';
-
-    ProfilePersonalInfoView.prototype.events = {
-      'click .radio': function(e) {
-        return $('#gender').val($('#' + e.target.id).val());
-      }
-    };
-
-    ProfilePersonalInfoView.prototype.onShow = function() {
-      this.$el.find("#timezone option[value='" + this.model.get('timezone') + "']").attr("selected", "selected");
-      $("input[name=radio_grp][value=" + this.model.get('gender') + "]").prop('checked', true);
-      $('#gender').val(this.model.get('gender'));
-      return jQuery.validator.addMethod("equalLength", function(value, element) {
-        return this.optional(element) || (parseInt(value.length) === 6);
-      }, "* Enter valid 6 digit Xooma ID");
-    };
-
-    $("#add_user_details").validate({
-      rules: {
-        xooma_member_id: {
-          number: true,
-          equalLength: true
-        },
-        phone_no: {
-          number: true
-        },
-        radio_grp: {
-          required: true
-        }
+  $("#add_user_details").validate({
+    rules: {
+      xooma_member_id: {
+        number: true,
+        equalLength: true
       },
-      submitHandler: function(form) {
-        $.ajax({
-          method: 'POST',
-          url: _SITEURL + '/wp-json/profiles/2',
-          data: $('#add_user_details').serialize(),
-          success: function(response) {
-            if (response.status === 404) {
-              return $('.response_msg').text(response.response);
-            } else {
-              return $('.response_msg').text("User details saved successfully");
-            }
-          },
-          error: function(error) {
-            return $('.response_msg').text("Details could not be saved");
-          }
-        });
-        return false;
+      phone_no: {
+        number: true
+      },
+      radio_grp: {
+        required: true
       }
-    });
+    },
+    submitHandler: function(form) {
+      $.ajax({
+        method: 'POST',
+        url: _SITEURL + '/wp-json/profiles/2',
+        data: $('#add_user_details').serialize(),
+        success: function(response) {
+          if (response.status === 404) {
+            return $('.response_msg').text(response.response);
+          } else {
+            return $('.response_msg').text("User details saved successfully");
+          }
+        },
+        error: function(error) {
+          return $('.response_msg').text("Details could not be saved");
+        }
+      });
+      return false;
+    }
+  });
 
-    return ProfilePersonalInfoView;
+  return ProfilePersonalInfoView;
 
-  })(Marionette.ItemView);
-
-}).call(this);
+})(Marionette.ItemView);
