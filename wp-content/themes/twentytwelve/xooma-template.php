4819ff375554344e9a83a328f229e39a57b1f932
Amit <amit@ajency.in>
2015-02-05 17:44:55 +0530

    zoom out feature added

--- wp-content/themes/twentytwelve/xooma-template.php
+++ wp-content/themes/twentytwelve/xooma-template.php
@@ -17,6 +17,7 @@
 
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title><?php wp_title( '|', true, 'right' ); ?></title>
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
