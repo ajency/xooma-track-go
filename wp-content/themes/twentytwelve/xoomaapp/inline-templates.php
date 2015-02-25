bede14decac3eeb77847b3bac33bc8a6558932d7
Amit <amit@ajency.in>
2015-02-25 09:55:51 +0530

    ui fix

--- wp-content/themes/twentytwelve/xoomaapp/inline-templates.php
+++ wp-content/themes/twentytwelve/xoomaapp/inline-templates.php
@@ -33,7 +33,7 @@
                     <h5 class="text-center"> Timely notification and email reminders<br>to support your product use and health goals</h5>
                     </div>
                     
-                    <div><h3> <img src="<?php echo get_template_directory_uri(); ?>/images/chart.png" alt="" class="center-block img-responsive" ></h3>
+                    <div><h3> <img src="<?php echo get_template_directory_uri(); ?>/images/fitness.png" alt="" class="center-block img-responsive" ></h3>
                     <h5 class="text-center">Measure the improvements <br> in your shape and size</h5>
                     </div>
                    <div><h3>   <img src="<?php echo get_template_directory_uri(); ?>/images/reminder.png " alt="" class="center-block img-responsive"  ></h3>
