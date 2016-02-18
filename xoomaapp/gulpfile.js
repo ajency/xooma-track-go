c06ddd30da04715a2140d12800cd46eace26ce92
Tanmai Naik <tanmai@ajency.in>
2016-02-18 12:56:57 +0530

    new

--- xoomaapp/gulpfile.js
+++ xoomaapp/gulpfile.js
@@ -1,18 +1,13 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
+    concat = require('gulp-concat'),
     coffee = require('gulp-coffee');
 
 var paths = {
     coffee: ['www/scripts/**/*.coffee'],
+    concat: ['www/production/**/**/*.js']
 };
 
-
-gulp.task('coffee', function() {
-    gulp.src(paths.coffee)
-        .pipe(coffee({
-            bare: true
-        }).on('error', gutil.log))
-});
 
 gulp.task('coffee', function() {
     gulp.src(paths.coffee)
@@ -22,7 +17,17 @@ gulp.task('coffee', function() {
         .pipe(gulp.dest('www/scripts/'));
 });
 
+gulp.task('concatfiles', function() {
+    gulp.src(paths.concat)
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('www/test/'));
+});
+
+
 
 gulp.task('watch', function() {
     gulp.watch(paths.coffee, ['coffee']);
 });
+
+
+gulp.task('concat',['concatfiles']);
