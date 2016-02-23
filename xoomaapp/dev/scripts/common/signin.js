208fa993c031fec71729feef4c7c0d74596aef87
Odorica <odorica@ajency.in>
2016-02-23 16:43:50 +0530

    changes to signIn

--- xoomaapp/dev/scripts/common/signin.js
+++ xoomaapp/dev/scripts/common/signin.js
@@ -1,5 +1,8 @@
-var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
-  hasProp = {}.hasOwnProperty;
+var SignInView,
+  extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+  hasProp = {}.hasOwnProperty,
+  bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
 App.state('SignIn', {
   url: '/signIn',
   parent: 'xooma'
@@ -28,6 +31,6 @@ App.SignInCtrl = (function(superClass) {
   };
 
 
-  return AddProductsCtrl;
+  return SignInCtrl;
 
 })(Ajency.RegionController);
