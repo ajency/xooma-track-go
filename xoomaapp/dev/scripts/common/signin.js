09a014e5cea4dc02776a3e4bdaab925dff9bed52
Odorica <odorica@ajency.in>
2016-02-23 16:26:01 +0530

    fdvcfvfd

---
+++ xoomaapp/dev/scripts/common/signin.js
@@ -0,0 +1,33 @@
+var extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+  hasProp = {}.hasOwnProperty;
+App.state('SignIn', {
+  url: '/signIn',
+  parent: 'xooma'
+});
+
+SignInView = (function(superClass) {
+  extend(SignInView, superClass);
+
+  function SignInView() {
+    return SignInView.__super__.constructor.apply(this, arguments);
+  }
+
+
+  SignInView.prototype.template = '#sign_in_template';
+
+  return SignInView;
+
+})(Marionette.ItemView);
+
+App.SignInCtrl = (function(superClass) {
+  extend(SignInCtrl, superClass);
+
+  SignInCtrl.prototype.initialize = function(options) {
+        return this.show(new SignInView();
+
+  };
+
+
+  return AddProductsCtrl;
+
+})(Ajency.RegionController);
