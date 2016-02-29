572dbce9f34ab4a2083ea8125ab3c059ff58eb84
Odorica <odorica@ajency.in>
2016-02-29 12:53:25 +0530

    redirection

--- wp-content/themes/twentytwelve/xoomaapp/scripts/app.js
+++ wp-content/themes/twentytwelve/xoomaapp/scripts/app.js
@@ -1,74 +1,89 @@
-jQuery(document).ready(function($) {
-  App.state('login').state('xooma', {
-    url: '/'
-  });
-  App.onBeforeStart = function() {
-    App.currentUser.set(window.userData);
-    if (!App.currentUser.isLoggedIn()) {
-      return App.currentUser.setNotLoggedInCapabilities();
-    }
-  };
-  App.currentUser.on('user:auth:success', function() {
-    if (window.isWebView()) {
-      window.userData = App.currentUser.toJSON();
-    }
-    App.trigger('fb:status:connected');
-    return App.navigate('#' + App.currentUser.get('state'), {
-      trigger: true,
-      replace: true
+(function() {
+  jQuery(document).ready(function($) {
+    App.state('login').state('xooma', {
+      url: '/'
     });
-  });
-  App.currentUser.on('user:logged:out', function() {
-    var arr;
-    arr = [];
-    App.useProductColl.reset(arr);
-    delete window.userData;
-    return App.navigate('#login', {
-      trigger: true,
-      replace: true
-    });
-  });
-  Offline.options = {
-    interceptRequests: true,
-    requests: true,
-    checks: {
-      xhr: {
-        url: "" + _SITEURL + "/"
+    App.onBeforeStart = function() {
+      App.currentUser.set(window.userData);
+      if (!App.currentUser.isLoggedIn()) {
+        return App.currentUser.setNotLoggedInCapabilities();
       }
-    }
-  };
-  Offline.on('confirmed-up', function() {
-    return $('.error-connection').css({
-      display: 'none'
+    };
+    App.currentUser.on('user:auth:success', function() {
+      if (window.isWebView()) {
+        window.userData = App.currentUser.toJSON();
+      }
+      App.trigger('fb:status:connected');
+      return App.navigate('#' + App.currentUser.get('state'), {
+        trigger: true,
+        replace: true
+      });
     });
-  });
-  Offline.on('confirmed-down', function() {
-    return $('.error-connection').css({
-      display: 'block'
+    App.currentUser.on('user:lauth:success', function() {
+      App.trigger('user:status:connected');
+      return App.navigate('#' + App.currentUser.get('state'), {
+        trigger: true,
+        replace: true
+      });
     });
+    App.currentUser.on('user:logged:out', function() {
+      var arr;
+      arr = [];
+      App.useProductColl.reset(arr);
+      delete window.userData;
+      return App.navigate('#login', {
+        trigger: true,
+        replace: true
+      });
+    });
+    Offline.options = {
+      interceptRequests: true,
+      requests: true,
+      checks: {
+        xhr: {
+          url: _SITEURL + "/"
+        }
+      }
+    };
+    Offline.on('confirmed-up', function() {
+      return $('.error-connection').css({
+        display: 'none'
+      });
+    });
+    Offline.on('confirmed-down', function() {
+      return $('.error-connection').css({
+        display: 'block'
+      });
+    });
+    App.addInitializer(function() {
+      return Backbone.history.start();
+    });
+    App.on('fb:status:connected', function() {
+      if (!App.currentUser.hasProfilePicture()) {
+        return App.currentUser.getFacebookPicture();
+      }
+    });
+    App.on('user:status:connected', function() {
+      if (!App.currentUser.hasProfilePicture()) {
+        return App.currentUser.getFacebookPicture();
+      }
+    });
+    App.on('cordova:register:push:notification', function() {
+      if (window.isWebView()) {
+        return Push.register();
+      }
+    });
+    App.on('cordova:set:user:data', function() {
+      if (window.isWebView()) {
+        return CordovaStorage.setUserData(App.currentUser.toJSON());
+      }
+    });
+    App.on('cordova:hide:splash:screen', function() {
+      if (window.isWebView()) {
+        return CordovaApp.hideSplashscreen();
+      }
+    });
+    return App.start();
   });
-  App.addInitializer(function() {
-    return Backbone.history.start();
-  });
-  App.on('fb:status:connected', function() {
-    if (!App.currentUser.hasProfilePicture()) {
-      return App.currentUser.getFacebookPicture();
-    }
-  });
-  App.on('cordova:register:push:notification', function() {
-    if (window.isWebView()) {
-      return Push.register();
-    }
-  });
-  App.on('cordova:set:user:data', function() {
-    if (window.isWebView()) {
-      return CordovaStorage.setUserData(App.currentUser.toJSON());
-    }
-  });
-  App.on('cordova:hide:splash:screen', function() {
-    if (window.isWebView()) {
-      return CordovaApp.hideSplashscreen();
-    }
-  });
-  return App.start();
-});
+
+}).call(this);
