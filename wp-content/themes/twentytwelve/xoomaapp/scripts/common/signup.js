e935f51f4a7523b68be872974d722df84aee493e
Odorica <odorica@ajency.in>
2016-03-01 17:45:23 +0530

    js changes

--- wp-content/themes/twentytwelve/xoomaapp/scripts/common/signup.js
+++ wp-content/themes/twentytwelve/xoomaapp/scripts/common/signup.js
@@ -1,8 +1,6 @@
-(function() {
-  var SignUpView,
-    bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
-    extend = function(child, parent) { for (var key in parent) { if (hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
-    hasProp = {}.hasOwnProperty;
+var FaqView,
+  __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
   App.state('SignUp', {
     url: '/signup',
@@ -142,4 +140,3 @@
 
   })(Ajency.RegionController);
 
-}).call(this);
